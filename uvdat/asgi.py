caf98a9617dc104ccee488360ccb9ed9cd9440e5
Bryon Lewis <Bryon.Lewis@kitware.com>
2025-02-07 08:10:14 -0500

    missing updates

--- uvdat/asgi.py
+++ uvdat/asgi.py
@@ -1,11 +1,26 @@
 import os
-
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 import configurations.importer
 from django.core.asgi import get_asgi_application
+from django.urls import path
+
+from uvdat.core.notifications import AppConsumer
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'uvdat.settings'
 if not os.environ.get('DJANGO_CONFIGURATION'):
     raise ValueError('The environment variable "DJANGO_CONFIGURATION" must be set.')
 configurations.importer.install()
 
-application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        'http': get_asgi_application(),
+        'websocket': AuthMiddlewareStack(
+            URLRouter(
+                [
+                    path('app/', AppConsumer.as_asgi(), name='app-ws'),
+                ]
+            )
+        ),
+    }
+)
